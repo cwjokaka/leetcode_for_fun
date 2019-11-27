3ae74f2fe5b19ab20ca16ba4f6c9d43fd84deda1
cwjokaka <1250217678@qq.com>
2019-11-27 09:58:05 +0800

    add btree

---
+++ Java/src/com/company/easy/Q_0104_MaximumDepthOfBinaryTree.java
@@ -0,0 +1,57 @@
+package com.company.easy;
+
+import com.company.common.TreeNode;
+import javafx.util.Pair;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Q_0104_MaximumDepthOfBinaryTree {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        root.right.left.left = new TreeNode(7);
+        System.out.println(new Q_0104_MaximumDepthOfBinaryTree().maxDepth(root));
+    }
+
+    public int maxDepth(TreeNode root) {
+//        return dfs(root);
+        return bfs(root);
+    }
+
+    // 深度优先
+    public int dfs(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftDepth = dfs(node.left);
+        int rightDepth = dfs(node.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+    // 广度优先(抄的)
+    public int bfs(TreeNode node) {
+        Queue<Pair<TreeNode, Integer>> queue = new LinkedList<>();
+        if (node != null) {
+            queue.add(new Pair(node, 1));
+        }
+
+        int depth = 0;
+        while (!queue.isEmpty()) {
+            // 弹出队首元素
+            Pair<TreeNode, Integer> current = queue.poll();
+            node = current.getKey();
+            int current_depth = current.getValue();
+            if (node != null) {
+                depth = Math.max(depth, current_depth);
+                queue.add(new Pair(node.left, current_depth + 1));
+                queue.add(new Pair(node.right, current_depth + 1));
+            }
+        }
+        return depth;
+    }
+}
